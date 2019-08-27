6866ba9b14e906f1640eb55b283856c85bc70c97
Cüneyt BOZYEL <cuneytbozyel@gmail.com>
2019-08-27 22:43:59 +0300

    set up api controller

---
+++ TestMaker/ViewModels/AnswerViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.ComponentModel;
+
+namespace TestMaker.ViewModels
+{
+    [JsonObject(MemberSerialization.OptOut)]
+    public class AnswerViewModel
+    {
+        #region Constructor
+        public AnswerViewModel()
+        {
+
+        }
+        #endregion
+
+        #region Properties
+        public int Id { get; set; }
+        public int QuizId { get; set; }
+        public int QuestionId { get; set; }
+        public string Text { get; set; }
+        public string Notes { get; set; }
+        [DefaultValue(0)]
+        public int Type { get; set; }
+        [DefaultValue(0)]
+        public int Flags { get; set; }
+        [DefaultValue(0)]
+        public int Value { get; set; }
+        [JsonIgnore]
+        public DateTime CreatedDate { get; set; }
+        public DateTime LastModifiedDate { get; set; }
+        #endregion
+    }
+}
