101bb3169270307159a8c6a3ea52bbd89b729c5b
Cüneyt BOZYEL <cuneytbozyel@gmail.com>
2019-08-29 22:23:42 +0300

    addto client answer, question and quiz components

--- TestMaker/ClientApp/src/app/app.module.ts
+++ TestMaker/ClientApp/src/app/app.module.ts
@@ -1,8 +1,10 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
@@ -15,6 +17,13 @@ import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { QuizEditComponent } from './quiz/quiz-edit.component';
+import { QuestionListComponent } from './question/question-list.component';
+import { QuestionEditComponent } from './question/question-edit.component';
+import { AnswerListComponent } from './answer/answer-list.component';
+import { AnswerEditComponent } from './answer/answer-edit.component';
+import { ResultListComponent } from './result/result-list.component';
+import { ResultEditComponent } from './result/result-edit.component';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -27,10 +36,17 @@ import { QuizEditComponent } from './quiz/quiz-edit.component';
     QuizEditComponent,
     AboutComponent,
     LoginComponent,
-    PagenotfoundComponent
+    PagenotfoundComponent,
+    QuestionListComponent,
+    QuestionEditComponent,
+    AnswerListComponent,
+    AnswerEditComponent,
+    ResultListComponent,
+    ResultEditComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    CommonModule,
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
@@ -39,6 +55,12 @@ import { QuizEditComponent } from './quiz/quiz-edit.component';
       { path: 'quiz/create', component: QuizEditComponent },
       { path: 'quiz/edit/:id', component: QuizEditComponent },
       { path: 'quiz/:id', component: QuizComponent },
+      { path: 'question/create/:id', component: QuestionEditComponent },
+      { path: 'question/edit/:id', component: QuestionEditComponent },
+      { path: 'answer/create/:id', component: AnswerEditComponent },
+      { path: 'answer/edit/:id', component: AnswerEditComponent },
+      { path: 'result/create/:id', component: ResultEditComponent },
+      { path: 'result/edit/:id', component: ResultEditComponent },
       { path: 'about', component: AboutComponent },
       { path: 'login', component: LoginComponent },
       { path: '**', component: PagenotfoundComponent }
